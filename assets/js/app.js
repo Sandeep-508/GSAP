f270741bfd9f051a765e7572b0fd328474c41adc
Sandeep <elsenothing321@gmail.com>
2024-08-17 11:48:00 +0530

    added changes in the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,37 @@
-gsap.fromTo(".box", { xPercent: "0", delay: "3" }, { xPercent: "640", duration: "2", color: "white", repeat: -1, yoyo: true, rotate: "360", backgroundColor: "yellow", ease: "power1.easeIn", yPercent: "250" })
-gsap.fromTo(".box2", { xPercent: "660", delay: "3", yPercent: "0" }, { xPercent: "0", yPercent: "250", duration: "2", color: "white", repeat: -1, yoyo: true, ease: "power1.easeIn", rotate: "360", backgroundColor: "blue" })
+// Box animations
+gsap.fromTo(".box",
+    { xPercent: 0, delay: 3 },
+    { xPercent: 640, duration: 2, color: "white", repeat: -1, yoyo: true, rotate: 360, backgroundColor: "yellow", ease: "power1.in", yPercent: 250 }
+);
 
+gsap.fromTo(".box2",
+    { xPercent: 660, delay: 3, yPercent: 0 },
+    { xPercent: 0, yPercent: 250, duration: 2, color: "white", repeat: -1, yoyo: true, ease: "power1.in", rotate: 360, backgroundColor: "blue" }
+);
+
+// Timeline animations
+gsap.registerPlugin(ScrollTrigger)
+var tl = gsap.timeline();
+
+tl.from(".text1", {
+    y: -40,
+    duration: 0.5,
+});
+tl.from("a", {
+    y: -40,
+    duration: 0.5,
+    stagger: 0.3,
+});
+tl.from(".centerText", {
+    opacity: 0,
+});
+tl.to(".scrollText", {
+    x: -2300,
+    scrollTrigger: {
+        trigger: ".scrollText",
+        start: "2% 0%",
+        end: "bottom 0%",
+        scrub: true,
+        pin: true,
+    }
+});
